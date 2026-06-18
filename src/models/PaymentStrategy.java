ca67ecce2bca6a38d2ecadc9f7bcea256c2b87d2
Namush Bui <nambnp30@gmail.com>
2026-06-18 21:15:04 +0700

    Delete src directory

--- src/models/PaymentStrategy.java
+++
@@ -1,6 +0,0 @@
-package models;
-
-
-public interface PaymentStrategy {
-    void pay(int amount);
-}
--- src/models/Subject.java
+++
@@ -1,7 +0,0 @@
-package models;
-
-public interface Subject {
-	 public void register(Observer o);
-	 public void unRegister(Observer o);
-	 public void notify(String nameMovies);
-}
